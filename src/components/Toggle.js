f291eb3d40ad26e42a0f169c8787bc992d0abca1
ahmadqaranyfarhan <ahmadqaranyfarhan5@gmail.com>
2024-05-24 18:08:06 +0300

    Completed Assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [change, setChange] = useState(false);
+  function handleChange() {
+    setChange((change) => !change);
+  }
+  return (
+    <>
+      <button
+        onClick={handleChange}
+        style={{ background: "#565ffa", border: "#565ffa" }}
+      >
+        {change ? "ON" : "OFF"}
+      </button>
+    </>
+  );
 }
 
 export default Toggle;
